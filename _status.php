93448f77e03eaf71245b8efd65afe59306f8115f
sohaibalam67 <sohaibalam67.gmail.com>
2017-11-29 16:06:07 +0530

    removed the redundant navigation bar code and made a seperate file for nav bar. Implemented the order cancelation functionality and admin and check the user login info.

--- _status.php
+++ _status.php
@@ -44,7 +44,6 @@ $conn=mysqli_connect('localhost','root','','shopdb');
 <html>
 <head>
 	<title>Order Status</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="UTF-8">
 	<link rel="icon" href="image/fevicon.png">
 	<link href="https://fonts.googleapis.com/css?family=Noto+Sans|Roboto:400,900" rel="stylesheet">
@@ -67,6 +66,31 @@ $conn=mysqli_connect('localhost','root','','shopdb');
       border-radius: 3px;
       padding:40px 40px 40px 40px;
     }
+
+    .ac_bt{
+      padding: 2px 6px 4px 6px;
+      border-radius: 3px;
+      box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.2);
+    }
+
+    .bt1{
+       background: #fdd053;
+    }
+
+    .bt2{
+       background: #a9dddf;
+    }
+ 
+
+    .bt3{
+       background: #adca63;
+    }
+
+    .bt4{
+       background: #e35d44;
+    }
+ 
+ 
  
     
 	
@@ -76,82 +100,10 @@ $conn=mysqli_connect('localhost','root','','shopdb');
 </head>
 <body>
 
-	<nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
-      <a class="navbar-brand" href="index.php"><img src="image/logo1.png"  height="35px" width="138px"></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item dp1">
-            <a class="nav-link" href="#">MEN </span></a>
-            <div class="dp_cont">
-            	
-            	<div class="container">
-            		<div class="row">
-   					<div class="col-sm-6">
-     				<div class="nav_title">TopWear</div>
-     				<div class="nav_cont"><a href="product.php?item=tshirt&gender=men">Tshirt</a> <br> <a href="product.php?item=casual_shirt&gender=men">Casual Shirt</a><br> <a href="product.php?item=formal_shirt&gender=men">Formal Shirt</a> <br> <a href="product.php?item=jacket&gender=men">Jacket</a> <br> <a href="product.php?item=blazer&gender=men">Blazer</a> <br> <a href="product.php?item=suit&gender=men">Suit</a></div>
-     				<div class="nav_title">BottomWear</div>
-     				<div class="nav_cont"><a href="product.php?item=jeans&gender=men">Jeans</a> <br> <a href="product.php?item=casual_trouser&gender=men">Casual Trouser</a> <br> <a href="product.php?item=formal_trouser&gender=men">Formal Trouser</a> <br> <a href="product.php?item=shorts&gender=men">Shorts</a> <br> <a href="product.php?item=track_pant&gender=men">Track Pants</a></div>
-    				</div>
-   					<div class="col-sm-6">
-      				<div class="nav_title">Sports & ActiveWear</div>
-     				<div class="nav_cont"><a href="product.php?item=active_tshirt&gender=men">Active Tshirt</a> <br> <a href="product.php?item=track_pant&gender=men">Track Pant</a> <br> <a href="product.php?item=shorts&gender=men">Shorts</a></div>
-     				<div class="nav_title">Indian and Festive Wear</div>
-     				<div class="nav_cont"><a href="product.php?item=kurta&gender=men">Kurta </a><br> <a href="product.php?item=shervani&gender=men">Shervani</a> <br> <a href="product.php?item=nehru_jacket&gender=men">Nehru Jacket</a></div>
-    				</div>
- 		 			</div>
-            	</div>
+	<?php
+  require('nav.html');
+  ?>
 
-
-            </div>
-          </li>
-          <li class="nav-item dp2">
-            <a class="nav-link" href="#">WOMEN</a>
-
-             <div class="dp_cont_2">
-            	
-            	<div class="container">
-            		<div class="row">
-   					<div class="col-sm-6">
-     				<div class="nav_title">Western Wear</div>
-     				<div class="nav_cont"><a href="product.php?item=tops&gender=women">Tops</a> <br> <a href="product.php?item=casual_shirt&gender=women">Casual Shirt </a><br> <a href="product.php?item=tshirt&gender=women">Tshirt</a> <br> <a href="product.php?item=jacket&gender=women">Jacket</a> <br> <a href="product.php?item=blazer&gender=women">Blazer</a> <br> <a href="product.php?item=dress&gender=women">Dresses</a></div>
-     				<div class="nav_title">BottomWear</div>
-     				<div class="nav_cont"><a href="product.php?item=jeans&gender=women">Jeans </a><br><a href="product.php?item=trouser&gender=women">Trouser </a><br> <a href="product.php?item=skirt&gender=women">Skirts</a><br> <a href="product.php?item=shorts&gender=women">Shorts</a></div>
-    				</div>
-   					<div class="col-sm-6">
-      				<div class="nav_title">Sports & ActiveWear</div>
-     				<div class="nav_cont"><a href="product.php?item=active_tshirt&gender=women">Active Tshirt</a> <br> <a href="product.php?item=track_pant&gender=women">Track Pant</a> <br> <a href="product.php?item=shorts&gender=women">Shorts</a></div>
-     				<div class="nav_title">Indian and Festive Wear</div>
-     				<div class="nav_cont"><a href="product.php?item=kurti&gender=women">Kurti </a><br><a href="product.php?item=suit&gender=women"> Suit </a><br></div>
-    				</div>
- 		 			</div>
-            	</div>
-
-
-            </div>
-          </li>
-          
-        </ul>
-        <form class="form-inline mt-2 mt-md-0"  style="margin-right: 10%">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" style="width: 400px">
-        </form>
-        <div class="dp3"><img src="image/account.png" height="25px" width="25px">
-        <div class="dp_cont_3">
-        <?php 
-          if(!isset($_SESSION["user"]) || $_SESSION["user"] == ""){
-            echo '<a href="login.php"><button class="btn btn-outline-warning">Login</button></a> <a href="register.php"><button class="btn btn-outline-info">Register</button></a>';
-          }else{
-            echo '<img src="image/face.png" height="20px" width="20px"> Hi! <i>'.$_SESSION["name"].'</i><br><br><a href="profile.php"><button class="btn btn-outline-info">Profile</button></a> <a href="logout.php"><button class="btn btn-outline-warning">Logout</button></a>';
-          }
-        ?>
-          
-        </div>
-        </div> &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp
-       <a href="bag.php"><img src="image/bag.png" height="25px" width="25px"></a>
-      </div>
-    </nav>
 
 
     <div class="inner_cont4">
@@ -179,13 +131,14 @@ $conn=mysqli_connect('localhost','root','','shopdb');
                   <td>qty</td>
                   <td>status</td>
                   <td>Action</td>
+                  <td>Print Address</td>
                 </tr>
 
 
                 <?php
                   foreach ($arr as $prodd) {
                     
-                    echo '<tr style="font-size:10pt">
+                    echo '<tr style="font-size:10pt;">
                   <td>'.$prodd["order_id"].'</td>
                   <td><img src="prod/'.$prodd["image"].'" height="133px" width="100px"></td>
                   <td>'.$prodd["title"].'</td>
@@ -212,8 +165,7 @@ $conn=mysqli_connect('localhost','root','','shopdb');
                   }
 
                   echo '</td>
-                  <td><a href="_update_order.php?od='.$prodd["order_id"].'&pi='.$prodd["prod_id"].'&act=0">Processing</a><br><a href="_update_order.php?od='.$prodd["order_id"].'&pi='.$prodd["prod_id"].'&act=1">Shipped</a><br><a href="_update_order.php?od='.$prodd["order_id"].'&pi='.$prodd["prod_id"].'&act=2">Delivered</a></td>
-                </tr>';
+                  <td><a href="_update_order.php?od='.$prodd["order_id"].'&pi='.$prodd["prod_id"].'&act=0" class="ac_bt bt1">Processing</a><br><br><a href="_update_order.php?od='.$prodd["order_id"].'&pi='.$prodd["prod_id"].'&act=1" class="ac_bt bt2">Shipped</a><br><br><a href="_update_order.php?od='.$prodd["order_id"].'&pi='.$prodd["prod_id"].'&act=2" class="ac_bt bt3">Delivered</a><br><br><a href="_cancel_order.php?id='.$prodd["order_id"].'&pid='.$prodd["prod_id"].'" class="ac_bt bt4">Cancel</a></td><td><a href="_print.php?id='.$prodd["order_id"].'">PRINT</a></td></tr>';
 
                   }
 
